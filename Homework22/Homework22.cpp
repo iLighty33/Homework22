a78622e3fc3998a2d957e96646e98e3f370feeae
iLighty33 <iLighty33@gmail.com>
2022-03-13 22:47:50 +0500

    Исправление ошибок

--- Homework22/Homework22.cpp
+++ Homework22/Homework22.cpp
@@ -3,42 +3,27 @@
 using namespace std;
 
 template <typename T> void mySwap(T* num1, T* num2);
-void myPower(int *n, int m);
+void myPower(int &n, int m);
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 
 	// Задача 1
 
-	int array[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	const int length = 10;
+	int array[length] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	cout << "Изначальный массив:\n[";
+	cout << "Задача 1.\nИзначальный массив:\n[";
 	int* pArr = array;
 	for (int i = 0; i < 10; i++)
 		cout << *(pArr + i) << ", ";
 	cout << "\b\b]\n";
 
-
 	cout << "Итоговый массив:\n[";
-	for (int i = 0; i < 1; i++) {
-		if (*(pArr) < *(pArr + 1)) 
-			mySwap(pArr, (pArr + 1));
-		
-		if (*(pArr + 2) < *(pArr+3))
-			mySwap(pArr + 2, (pArr + 3));
 
-		if (*(pArr + 4) < *(pArr + 5))
-			mySwap(pArr + 4, (pArr + 5));
-
-		if (*(pArr + 6) < *(pArr + 7))
-			mySwap(pArr + 6, (pArr + 7));
-
-		if (*(pArr + 8) < *(pArr + 9))
-			mySwap(pArr + 8, (pArr + 9));
-
-		if (*(pArr + 9) < *(pArr + 10))
-			mySwap(pArr + 9, (pArr + 10));
-
+	for (int i = 0; i < length; i++) {
+		if (i % 2 == 0)
+			mySwap((pArr + i), (pArr + i + 1));
 	}
 
 	for (int i = 0; i < 10; i++)
@@ -47,34 +32,37 @@ int main() {
 
 	// Задача 2
 
-	int n = 10;
-	myPower(&n, 3);
+	cout << "\nЗадача 2.\n";
+	cout << "Введите число: ";
+	int n, m;
+	int& refN = n;
+	cin >> n;
+	cout << "Введите степень: ";
+	cin >> m;
+
+	myPower(refN, m);
+	cout << "Результат: ";
 	cout << n << endl;
 
 	return 0;
 }
 
-void myPower(int* num1, int m) {
+void myPower(int& num1, int m) {
 	if (m == 0) {
 		cout << 1 << endl;
 		return;
 	}
-	int nTemp = *num1;
+	int nTemp = num1;
 	int number = 1;
-	num1 = &number;
+	num1 = number;
 
-	
 	for (int i = 1; i <= m; i++) {
 		
-		*num1 = *num1 *  nTemp;
+		num1 = num1 *  nTemp;
 
 	}
 
-	cout << *num1 << endl;
-
-	
 	return;
-	
 
 }
 
